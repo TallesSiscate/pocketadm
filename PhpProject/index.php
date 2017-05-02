c93cde2708bccdad97450558aa1063c054cf0414
Talles <Talles@ACERI3-TALLES>
2017-05-02 06:27:37 -0300

    Adicionados codigos.

---
+++ PhpProject/index.php
@@ -0,0 +1,26 @@
+﻿<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Hello Pocket!</title>
+</head>
+
+<body>
+	<?php
+		echo 'Bem-Vindo Administrador';
+		echo "<br /><br />";
+	?>
+	<form name="searchform" method="post" action="busca.php"><!-- action para o resultado em uma nova página -->
+		Página de busca: 
+		<input type="submit" value="Ir" />
+	</form>
+	<?php
+		echo "<br /><br />";
+	?>
+	<form name="searchform" method="post" action="cadastro.php"><!-- action para o resultado em uma nova página -->
+		Página de cadastro: 
+		<input type="submit" value="Ir" />
+	</form>
+</body>
+
+</html>
